77f8aac7ac1cfb2f1eb2653119eb472450629cc8
Julian Weaver <julian@hypo.io>
2015-11-12 08:48:51 +0000

    Converted to es6

--- server/react-processor.js
+++ server/react-processor.js
@@ -1,60 +1,47 @@
-"use strict";
-var factory = require("./react-factory");
-var getModule = require("./react-require");
+'use strict';
+import factory from './react-factory';
+import getModule from './react-require';
 
 function prepareName(name) {
-  return [
-    name.substr(0, 1).toUpperCase(),
-    name.substr(1).toLowerCase()
-  ].join("");
+    return [
+        name.substr(0, 1).toUpperCase(),
+        name.substr(1).toLowerCase()
+    ].join('');
 }
 
 function getName(pattern) {
-  return function(name) {
-    return prepareName(name.replace(pattern, "$1"));
-  };
+    return function (name) {
+        return prepareName(name.replace(pattern, '$1'));
+    };
 }
 
-var getCmpName = getName(/.*\/(.*)\..*/i);
+const getCmpName = getName(/.*\/(.*)\..*/i);
 
-var processor = {
-  getComponent: function(cmpUrl, assets, callback) {
-    var cmpPath = [
-      assets.toLowerCase(), cmpUrl
-    ].join("");
-
-    getModule.import(cmpPath, function(cmp) {
-
-      if (typeof cmp === "object") {
-        cmp = cmp[getCmpName(cmpUrl)];
-      }
-
-      callback(cmp);
-    });
-  },
-  run: function(getProperty, callback) {
-    var
-      response = null,
-      cmp = getProperty("cmp"),
-      data = getProperty("params"),
-      assets = getProperty("assets");
-
-    if (data) {
-      data = JSON.parse(data);
+const processor = {
+    getComponent(cmpUrl, assets, callback) {
+        const cmpPath = [
+            assets.toLowerCase(),
+            cmpUrl
+        ].join('');
+        
+        getModule.import(cmpPath, cmp => {
+            if (typeof cmp === 'object') {
+                cmp = cmp[getCmpName(cmpUrl)];
+            }
+            callback(cmp);
+        });
+    },
+    run(getProperty, callback) {
+        let response = null, cmp = getProperty('cmp'), data = getProperty('params'), assets = getProperty('assets');
+        if (data) {
+            data = JSON.parse(data);
+        }
+        this.getComponent(cmp, assets, _component => {
+            if (_component) {
+                response = factory.render(_component, data, +getProperty('static'));
+            }
+            return callback(response);
+        });
     }
-
-    // console.log(cmp);
-
-    this.getComponent(cmp, assets, function(_component) {
-      if (_component) {
-        response = factory.render(_component, data, +getProperty("static"));
-      }
-
-      return callback(response);
-
-    });
-
-  }
 };
-
 module.exports = processor;
