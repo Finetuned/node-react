107ab28ecd69e8d8e9d5719aefc21a37cf1d42b2
Julian Weaver <julian@hypo.io>
2015-11-12 08:56:01 +0000

    Updated to es6 Added util for deep object inspection getElement() Expanded getElement to perform isValidElement check
    module.exports:
    render()
    corrected scope of getElement()

--- server/react-factory.js
+++ server/react-factory.js
@@ -1,23 +1,28 @@
-"use strict";
-var React = require("react");
+'use strict';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import util from 'util';
 
 function getElement(_class, props) {
-  return React.createElement(_class, props || {});
+  const el = React.createElement(_class, props);
+  if (!React.isValidElement(el)) {
+    console.log('getElement error: ' + util.inspect(p, {showHidden: true, depth: null}));
+  }
+  return el;
 }
 
 function renderMarkup(_static) {
-  var method = "renderToString";
+  let method = 'renderToString';
   if (_static) {
-    method = "renderToStaticMarkup";
+    method = 'renderToStaticMarkup';
   }
-  return function(element) {
-    return React[method](element);
+  return function (element) {
+    return ReactDOMServer[method](element);
   };
 }
-
 module.exports = {
   getElement: getElement,
-  render: function(_class, props, _static) {
-    return renderMarkup(_static)(getElement(_class, props));
+  render(_class, props, _static) {
+    return renderMarkup(_static)(this.getElement(_class, props));
   }
 };
